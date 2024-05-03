fe88bc587a55d29780aa131b2fd9c834e249a38b
Taisuke Fukuno <fukuno@jig.jp>
2024-05-04 06:42:38 +0900

    add key

--- download_audio.js
+++ download_audio.js
@@ -4,10 +4,12 @@ import { fetchBin } from "https://js.sabae.cc/fetchBin.js";
 const url = "https://dl.ndl.go.jp/contents/1313607/704f12ff-f8fc-4fec-8002-58b60d0d5355/b6c669bd-91c9-4ea5-95cc-1cfeec2c4344/b6c669bd-91c9-4ea5-95cc-1cfeec2c4344_hls.m3u8";
 const txt = await fetchOrLoad(url);
 const fn = url.substring(url.lastIndexOf("/") + 1);
-await Deno.writeTextFile(fn, txt);
+await Deno.writeTextFile("audio/" + fn, txt);
 const ss = txt.split("\n");
-const path = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
+const path = "audio/" + url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
 const baseurl = url.substring(0, url.lastIndexOf("/") + 1);
+const key = await fetchBin(baseurl + "encrypt.key");
+await Deno.writeFile(path + "/encrypt.key", key);
 await Deno.mkdir(path, { recursive: true });
 for (const s of ss) {
   if (s.length == 0 || s.startsWith("#")) continue;
