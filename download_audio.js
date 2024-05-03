d98b44d793be3967bec550d83593abec73662725
Taisuke Fukuno <fukuno@jig.jp>
2024-05-04 07:08:51 +0900

    add src link

--- download_audio.js
+++ download_audio.js
@@ -1,6 +1,7 @@
 import { fetchOrLoad } from "https://js.sabae.cc/fetchOrLoad.js";
 import { fetchBin } from "https://js.sabae.cc/fetchBin.js";
 
+// https://ndlsearch.ndl.go.jp/books/R100000039-I1313607
 const url = "https://dl.ndl.go.jp/contents/1313607/704f12ff-f8fc-4fec-8002-58b60d0d5355/b6c669bd-91c9-4ea5-95cc-1cfeec2c4344/b6c669bd-91c9-4ea5-95cc-1cfeec2c4344_hls.m3u8";
 const baseurl = url.substring(0, url.lastIndexOf("/") + 1);
 const path = "audio/" + url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
